160ed85c1a6dad3946958cf93b5f889ce8732b5e
Andrés <andresprietoyanes@gmail.com>
2019-11-25 09:03:22 +0000

    Update cli.py cold

--- src/cli.py
+++ src/cli.py
@@ -2,6 +2,8 @@ from mqtt import MqttListener
 from ledstrip import LedStrip
 
 ledstrip = LedStrip()
+while True:
+    ledstrip.set_profile("Cold")
 mqttlistener = MqttListener(ledstrip)
 ledstrip.pixel_strip.getPixels()
 
