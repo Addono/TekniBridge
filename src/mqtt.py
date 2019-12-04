59826ea529523c24adfd085fb8c37cddae025630
Adriaan Knapen <hi@aknapen.nl>
2019-12-04 20:15:07 +0100

    :rocket: Switch to broker.hivemq.com

--- src/mqtt.py
+++ src/mqtt.py
@@ -15,7 +15,7 @@ class MqttListener:
         """
         self.led_strip = led_strip
 
-        self.host = "test.mosquitto.org"
+        self.host = "broker.hivemq.com"
         self.port = 1883
         client_id = "tek-" + str(random.randint(0, 10000000))
 
