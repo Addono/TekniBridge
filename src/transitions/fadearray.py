59d4951829466c11c51cbfd969485994ac31bc26
andrespy <andresprietoyanes@gmail.com>
2019-12-09 15:14:30 +0100

    refactor: remove commented out code

--- src/transitions/fadearray.py
+++ src/transitions/fadearray.py
@@ -8,8 +8,4 @@ class FadeArray(AbstractTransition):
         self.targets = None
 
     def step(self, previous):
-        # if len(previous) != len(self.targets):
-        #    print("Dimension mismatch between previous (%s) and targets (%s) " % (len(previous), len(self.targets)))
-        #   self.targets = [Led(*[random(), random(), random()]) for _ in range(len(previous))]
-
         return [led_previous.blend(led_target, self.rate) for led_previous, led_target in zip(previous, self.targets)]
