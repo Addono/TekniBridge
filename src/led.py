f84d7341cbd16254560a4df9b52d7855968ed505
Adriaan Knapen <hi@aknapen.nl>
2020-01-08 09:00:57 +0100

    💡 Add method and class documentation to Led

--- src/led.py
+++ src/led.py
@@ -1,4 +1,10 @@
 class Led:
+    """
+    Represents an individual RGB LED. 
+    
+    The implementation aims to be immutable, although fields are left public. This could
+    change in future versions.
+    """
 
     def __init__(self, red: float, green: float, blue: float, brightness: float = 1.0):
         super().__init__()
@@ -16,10 +22,14 @@ class Led:
 
     def __eq__(self, other):
         """Overrides the default implementation"""
-        return (self.red == other.red and self.blue == other.blue and self.green == other.green and self.brightness
-                == other.brightness)
+        return (
+            self.red == other.red
+            and self.blue == other.blue
+            and self.green == other.green
+            and self.brightness == other.brightness
+        )
 
-    def with_brightness(self, brightness: float):
+    def with_brightness(self, brightness: float) -> 'Led':
         """
         Creates a new LED instance with the same colors, but new brightness
         @param brightness: The brightness of the new LED object
@@ -27,17 +37,32 @@ class Led:
         """
         return Led(self.red, self.green, self.blue, brightness)
 
-    def similar(self, other, disparity=0.05, only_color=False):
-        return (abs(self.red - other.red) < disparity and abs(self.green - other.green) < disparity and
-                abs(self.blue - other.blue) < disparity and (only_color or abs(self.brightness - other.brightness) < disparity))
+    def similar(self, other: 'Led', disparity: float = 0.05, only_color: bool = False) -> bool:
+        return (
+            abs(self.red - other.red) < disparity
+            and abs(self.green - other.green) < disparity
+            and abs(self.blue - other.blue) < disparity
+            and (only_color or abs(self.brightness - other.brightness) < disparity)
+        )
 
     def __str__(self) -> str:
         return "|".join(map(lambda x: str(round(x * 100)), [self.red, self.green, self.blue, self.brightness]))
 
-    def scale(self, alpha: float):
+    def scale(self, alpha: float) -> 'Led':
+        """
+        Create a linearly scaled instance of this LED.
+        """
+        assert 0 <= alpha <= 1
+
         scaled_values = (alpha * v for v in self)
         return Led(*scaled_values)
 
-    def blend(self, other: 'Led', alpha: float):
+    def blend(self, other: 'Led', alpha: float) -> 'Led':
+        """
+        Blends - linearly scales - between two Leds. Returns self when alpha is zero, returns the other LED if
+        alpha is one.
+        """
+        assert 0 <= alpha <= 1
+
         mixed_values = ((1 - alpha) * v1 + alpha * v2 for v1, v2 in zip(self, other))
         return Led(*mixed_values)
