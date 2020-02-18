3f7ae3af2b00cc1ff6cfa515aaed44ff791c412d
Adriaan Knapen <hi@aknapen.nl>
2020-02-18 13:43:59 +0100

    🏷️  Fix typehinting for transitions.FadeArray

--- src/transitions/fadearray.py
+++ src/transitions/fadearray.py
@@ -1,11 +1,28 @@
+from typing import List, Optional
+
 from transitions import AbstractTransition
+from led import Led
 
 
 class FadeArray(AbstractTransition):
 
     def __init__(self, rate: float = 0.01) -> None:
         self.rate = rate
-        self.targets = None
+        self._targets: List[Led] = []
 
-    def step(self, previous):
-        return [led_previous.blend(led_target, self.rate).with_brightness(self.brightness) for led_previous, led_target in zip(previous, self.targets)]
+    @property
+    def targets(self):
+        return self._targets
+    
+    @targets.setter
+    def targets(self, targets: List[Led]):
+        assert len(targets) > 0
+
+        self._targets = targets
+
+    def step(self, previous: List[Led]) -> List[Led]:
+        # Do not do anything when targets is not yet set
+        if not self._targets:
+            return previous
+        
+        return [led_previous.blend(led_target, self.rate).with_brightness(self.brightness) for led_previous, led_target in zip(previous, self._targets)]
